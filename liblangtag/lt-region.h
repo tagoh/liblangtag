57ffe6c481b1828c0ee9cbf33e642751f432615b
Akira TAGOH <akira@tagoh.org>
2012-01-06 12:07:03 +0900

    Add an API to obtain the shortest code for region

--- liblangtag/lt-region.h
+++ liblangtag/lt-region.h
@@ -37,13 +37,14 @@ typedef struct _lt_region_t	lt_region_t;
 typedef enum _lt_region_code_t	lt_region_code_t;
 
 
-lt_region_t *lt_region_ref             (lt_region_t       *region);
-void         lt_region_unref           (lt_region_t       *region);
-gboolean     lt_region_has_alpha_2_code(const lt_region_t *region);
-gboolean     lt_region_has_alpha_4_code(const lt_region_t *region);
-const gchar *lt_region_get_name        (const lt_region_t *region);
-const gchar *lt_region_get_code        (const lt_region_t *region,
-                                        lt_region_code_t   code_type);
+lt_region_t *lt_region_ref              (lt_region_t       *region);
+void         lt_region_unref            (lt_region_t       *region);
+gboolean     lt_region_has_alpha_2_code (const lt_region_t *region);
+gboolean     lt_region_has_alpha_4_code (const lt_region_t *region);
+const gchar *lt_region_get_name         (const lt_region_t *region);
+const gchar *lt_region_get_code         (const lt_region_t *region,
+                                         lt_region_code_t   code_type);
+const gchar *lt_region_get_shortest_code(const lt_region_t *region);
 
 G_END_DECLS
 
