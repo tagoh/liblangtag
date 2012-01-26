baddf29533a77f9bb74d7e0f189e8c2f52a9adfc
Akira TAGOH <akira@tagoh.org>
2012-01-26 20:36:41 +0900

    missing function for redundant handlers

--- liblangtag-gobject/lt-gobject.h
+++ liblangtag-gobject/lt-gobject.h
@@ -32,6 +32,8 @@ G_BEGIN_DECLS
 #define LT_TYPE_GRANDFATHERED		lt_grandfathered_get_type()
 #define LT_TYPE_LANG_DB			lt_lang_db_get_type()
 #define LT_TYPE_LANG			lt_lang_get_type()
+#define LT_TYPE_REDUNDANT_DB		lt_redundant_db_get_type()
+#define LT_TYPE_REDUNDANT		lt_redundant_get_type()
 #define LT_TYPE_REGION_DB		lt_region_db_get_type()
 #define LT_TYPE_REGION			lt_region_get_type()
 #define LT_TYPE_SCRIPT_DB		lt_script_db_get_type()
@@ -41,19 +43,21 @@ G_BEGIN_DECLS
 #define LT_TYPE_VARIANT			lt_variant_get_type()
 
 
-GType lt_extlang_db_get_type(void);
-GType lt_extlang_get_type(void);
+GType lt_extlang_db_get_type      (void);
+GType lt_extlang_get_type         (void);
 GType lt_grandfathered_db_get_type(void);
-GType lt_grandfathered_get_type(void);
-GType lt_lang_db_get_type(void);
-GType lt_lang_get_type(void);
-GType lt_region_db_get_type(void);
-GType lt_region_get_type(void);
-GType lt_script_db_get_type(void);
-GType lt_script_get_type(void);
-GType lt_tag_get_type(void);
-GType lt_variant_db_get_type(void);
-GType lt_variant_get_type(void);
+GType lt_grandfathered_get_type   (void);
+GType lt_lang_db_get_type         (void);
+GType lt_lang_get_type            (void);
+GType lt_redundant_db_get_type    (void);
+GType lt_redundant_get_type       (void);
+GType lt_region_db_get_type       (void);
+GType lt_region_get_type          (void);
+GType lt_script_db_get_type       (void);
+GType lt_script_get_type          (void);
+GType lt_tag_get_type             (void);
+GType lt_variant_db_get_type      (void);
+GType lt_variant_get_type         (void);
 
 G_END_DECLS
 
