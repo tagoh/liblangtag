7eadc2a2da29e526c6f7d5f4840b8e132a5be597
Akira TAGOH <akira@tagoh.org>
2012-09-21 13:30:39 +0900

    Changed to the dual-licensing with MPLv2

--- liblangtag/lt-ext-module-private.h
+++ liblangtag/lt-ext-module-private.h
@@ -6,18 +6,9 @@
  * Authors:
  *   Akira TAGOH  <akira@tagoh.org>
  * 
- * This library is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You may distribute under the terms of either the GNU
+ * Lesser General Public License or the Mozilla Public
+ * License, as specified in the README file.
  */
 #ifndef __LT_EXT_MODULE_PRIVATE_H__
 #define __LT_EXT_MODULE_PRIVATE_H__
--- liblangtag/lt-extlang.h
+++ liblangtag/lt-extlang.h
@@ -6,18 +6,9 @@
  * Authors:
  *   Akira TAGOH  <akira@tagoh.org>
  * 
- * This library is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You may distribute under the terms of either the GNU
+ * Lesser General Public License or the Mozilla Public
+ * License, as specified in the README file.
  */
 #if !defined (__LANGTAG_H__INSIDE) && !defined (__LANGTAG_COMPILATION)
 #error "Only <liblangtag/langtag.h> can be included directly."
